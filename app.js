161bacddd6467a7fccddbf4146dbf83079d9047f
KaliGautier <gautieresam@gmail.com>
2020-07-03 10:06:23 +0200

    debug spotdl V2.00

--- app.js
+++ app.js
@@ -5,13 +5,13 @@
     var querystring = require('querystring');
     var cookieParser = require('cookie-parser');
 
-    var client_id = ''; // Your client id
-    var client_secret = ''; // Your secret
+    var client_id = '712d661608f94d3e916982f83b9e6f36'; // Your client id
+    var client_secret = 'b1505158b78942a6b167f93f3c2a4434'; // Your secret
     var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 
     var exec = require('child_process').exec, child;
-    var nodemailer = require('nodemailer'); 
+    var nodemailer = require('nodemailer');
     var moveFile = require('move-file');
     var fs = require('fs');
     var fc = require('fs-extra');
@@ -19,7 +19,6 @@
     var archiver = require('archiver');
     var zipper = require('zip-local');
     var lienUser;
-
 
 /**
 *   Generates a random string containing numbers and letters
@@ -33,7 +32,7 @@
         for (var i = 0; i < length; i++) {
             text += possible.charAt(Math.floor(Math.random() * possible.length));
         }
-        
+
         return text;
     };
 
@@ -71,15 +70,14 @@
         var storedState = req.cookies ? req.cookies[stateKey] : null;
 
     if (state === null || state !== storedState) {
-    
+
         res.redirect('/#' +
             querystring.stringify({
             error: 'state_mismatch'
         }));
-    
-    } else {
 
-        res.clearCookie(stateKey);
+      } else {
+      res.clearCookie(stateKey);
         var authOptions = {
             url: 'https://accounts.spotify.com/api/token',
             form: {
@@ -94,7 +92,7 @@
         };
 
     request.post(authOptions, function(error, response, body) {
-    
+
     if (!error && response.statusCode === 200) {
 
         var access_token = body.access_token,
@@ -141,7 +139,7 @@ app.get('/refresh_token', function(req, res) {
     };
 
     request.post(authOptions, function(error, response, body) {
-    
+
     if (!error && response.statusCode === 200) {
         var access_token = body.access_token;
         res.send({
@@ -150,12 +148,11 @@ app.get('/refresh_token', function(req, res) {
     }});
 });
 
-
 app.get('/dlplaylist', function(req, res) {
 
     var refresh_token = req.query.refresh_token;
     var authOptions = {
-    
+
         url: 'https://accounts.spotify.com/api/token',
         headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
         form: {
@@ -170,42 +167,73 @@ app.get('/dlplaylist', function(req, res) {
     var lienDeDl;
 
     const { exec } = require('child_process');
+
+        // Creation d'un fichier qui est dans le repertoire courant.
+        // Le fichier contient la liste des musiques sous le format :
+        // https://open.spotify.com/track/5csnxERQgKm1eAEvJI7NRw
+        // La commande exec crée un processus fils et lance la commande spotdl.
+
         exec('spotdl --playlist='+urlSpotify, (err, stdout, stderr) => {
             if (err) {
+
                 console.error(err)
+                // Traiter le cas avec une erreur.
+
             } else {
-    
+                console.log(urlSpotify)
                 // the *entire* stdout and stderr (buffered)
                 console.log(`stdout: ${stdout}`);
                 console.log(`stderr: ${stderr}`);
 
-                reponseBash = `${stderr}`; 
+                reponseBash = `${stderr}`;
                 console.log(reponseBash);
 
-    const nameFileDl = reponseBash.split(' '); // INFO: Writing 12 tracks to birthday-bob-sinclar.txt
-    const sansLePointTxtnameFileDl= nameFileDl[5].split('.');
-    var numeroUniquePlaylist = generateRandomString(20);
-    var sansLePointTxtnameFileDlavecNumero=numeroUniquePlaylist;
-    sansLePointTxtnameFileDlavecNumero=sansLePointTxtnameFileDlavecNumero.concat(sansLePointTxtnameFileDl[0]);
-    copierColler(nameFileDl[5],numeroUniquePlaylist);
-    creationRepertoirePlaylist(sansLePointTxtnameFileDlavecNumero);
-    downloadMusicPlaylist(sansLePointTxtnameFileDlavecNumero);
+                // Si il n'y a pas d'erreur spotdl retourne ceci : "NFO: Writing 12 tracks to <nomDeLaPlaylist>.txt"
+                const nameFileDl = reponseBash.split(' '); // INFO: Writing 12 tracks to birthday-bob-sinclar.txt
+
+                const sansLePointTxtnameFileDl= nameFileDl[5].split('.'); // On isole le <nomDeLaPlaylist> sans l'extension
+
+                // Creation d'un numero unique, si 2 personnes veulent télècharger une playlist avec un même nom.
+                var numeroUniquePlaylist = generateRandomString(20);
+
+
+
+                // Création d'une variable sous le format : <numeroUnique><nomDeLaPlaylist>
+                var sansLePointTxtnameFileDlavecNumero=numeroUniquePlaylist;
+                sansLePointTxtnameFileDlavecNumero=sansLePointTxtnameFileDlavecNumero.concat(sansLePointTxtnameFileDl[0]);
+
+                console.log("sansLePointTxtnameFileDlavecNumero")
+                console.log(sansLePointTxtnameFileDlavecNumero);
+
+                // Le fichier créé par spotdl est déplacé dans list
+                copierColler(nameFileDl[5],numeroUniquePlaylist); // good
+
+                // Un repertoire dans playlist va être créé.
+                // Format du nom du repertoire : <numeroUnique><nomDeLaPlaylist>
+                creationRepertoirePlaylist(sansLePointTxtnameFileDlavecNumero);
+
+                // Télèchargement
+                downloadMusicPlaylist(sansLePointTxtnameFileDlavecNumero);
+
+// Revoir indentation
 
     function downloadMusicPlaylist(fileName){
-    
-        exec('spotdl --folder ./playlists/'+fileName+' --list=./list/'+fileName+'.txt', (err, stdout, stderr) => {
-           
+
+          exec('spotdl --list=./list/'+fileName+'.txt -f ./playlists/'+fileName, (err, stdout, stderr) => {
+
+
             if (err) {
+                console.log("error spotdl --folder ./playlists/.....")
                 console.error(err)
             } else {
-                
+
                 console.log(`stdout: ${stdout}`);
                 console.log(`stderr: ${stderr}`);
+
                 createZip(fileName);
-                console.log("envoyer mail");
                 envoyerMail();
                 SupprimerLaPlaylistDuRepertoire(fileName);
-                supprimerLeFichierTxt(fileName); 
+                supprimerLeFichierTxt(fileName);
             }
         });
     }
@@ -213,12 +241,13 @@ app.get('/dlplaylist', function(req, res) {
 
 
     function supprimerLeFichierTxt(fileName){
-        
-        // Etape 1 : suppresion dans le repertoire list  
+
+        // Etape 1 : suppresion dans le repertoire list
         console.log("Supprimer le fichier txt dans list")
+        console.log(fileName)
         const { exec } = require('child_process');
         exec('rm ./list/'+fileName+'.txt', (err, stdout, stderr) => {
-            
+
             if (err) {
                 //some err occurred
                 console.error(err)
@@ -228,6 +257,8 @@ app.get('/dlplaylist', function(req, res) {
                 console.log(`stderr: ${stderr}`);
             }
         });
+
+
     }
 
 
@@ -235,7 +266,71 @@ app.get('/dlplaylist', function(req, res) {
 
         const { exec } = require('child_process');
         exec('mkdir ./playlists/'+nameFile, (err, stdout, stderr) => {
-            
+
+            if (err) {
+                //some err occurred
+                console.error(err)
+                // Gérer le cas ou il y a une erreur..
+            } else {
+                // the *entire* stdout and stderr (buffered)
+                console.log(`stdout: ${stdout}`);
+                console.log(`stderr: ${stderr}`);
+            }
+        });
+    }
+
+    function copierColler(nameFile,numeroUnique){
+
+        console.log("function copierColler")
+        console.log(numeroUnique)
+        console.log(nameFile)
+
+        var currentPath ="";
+        var newPath ="";
+
+        // Enlever le retour chariot le remplacer par un espace
+        var nameFile2=nameFile.replace(/\n|\r|(\n\r)/g,' ');
+        var nameFile=nameFile2;
+
+        // Diviser le namefile par le caractere espace
+        nameFile2 = nameFile.split(' '); // INFO: Writing 12 tracks to birthday-bob-sinclar.txt
+
+        // Recuperer que le nom sans l'espace : exemple a.txt.<espace>
+        nameFile=nameFile2[0];
+
+        // Il faut Enlever le point après le mot "txt"
+        nameFile=nameFile2[0].substring(0,nameFile2[0].length-1);
+
+        // Creation de la source avec le numero unique
+        var src = currentPath.concat(nameFile);
+        var tmp = newPath.concat("./list/");
+        var tmpDest = tmp.concat(numeroUnique);
+
+        var dest = tmpDest.concat(nameFile);
+
+        // Affichage
+        console.log('currentPath :'+src);
+        console.log('newPath :'+ dest);
+
+        const { exec } = require('child_process');
+        exec('cp '+src+' '+dest, (err, stdout, stderr) => {
+
+            if (err) {
+                //some err occurred
+                console.error(err)
+                // Gérer le cas ou il y a une erreur..
+            } else {
+                // the *entire* stdout and stderr (buffered)
+                console.log(`stdout: ${stdout}`);
+                console.log(`stderr: ${stderr}`);
+            }
+        });
+
+        console.log("Supprimer le fichier txt dans list")
+
+        console.log("fileNameCopy="+src)
+        exec('rm '+src, (err, stdout, stderr) => {
+
             if (err) {
                 //some err occurred
                 console.error(err)
@@ -244,45 +339,9 @@ app.get('/dlplaylist', function(req, res) {
                 console.log(`stdout: ${stdout}`);
                 console.log(`stderr: ${stderr}`);
             }
-        }); 
-    }
+        });
 
-    function copierColler(nameFile,numeroUnique){
 
-        console.log(numeroUnique)
-        console.log(nameFile)
-    
-        var currentPath ="";
-        var newPath ="";
-
-        // Enlever le retour chariot le remplacer par un espace 
-        var nameFile2=nameFile.replace(/\n|\r|(\n\r)/g,' ');
-        var nameFile=nameFile2;
-        
-        // Diviser le namefile par le caractere espace 
-        nameFile2 = nameFile.split(' '); // INFO: Writing 12 tracks to birthday-bob-sinclar.txt
-        
-        // Recuperer que le nom sans l'espace : exemple a.txt<espace> 
-        nameFile=nameFile2[0];
-
-        // Creation de la source avec le numero unique 
-        var src = currentPath.concat(nameFile);
-        var tmp = newPath.concat("./list/");
-        var tmpDest = tmp.concat(numeroUnique);
-
-        var dest = tmpDest.concat(nameFile);
-
-        // Affichage 
-        console.log('currentPath :'+src);
-        console.log('newPath :'+ dest);
-
-        // Deplacement des fichiers 
-        var moveFile = require('move-file');
-
-        (async () => {
-        await moveFile(src, dest);
-        console.log('The file has been moved');
-        })();
 
     }
 
@@ -296,7 +355,7 @@ app.get('/dlplaylist', function(req, res) {
     }
 
     function envoyerMail(){
-        
+
         var transporter = nodemailer.createTransport({
             service: 'Gmail',
             auth: {
@@ -309,11 +368,11 @@ app.get('/dlplaylist', function(req, res) {
             from: 'spotifyappdl@gmail.com',
             to: 'gautieresam@gmail.com',
             subject: 'spotifyappdl votre télèchargement est prêt ! ',
-            text: 'Votre télèchargement est prêt ! Voici le lien ! 192.168.1.6:8888/'+lienUser+'.zip',
+            text: 'Votre télèchargement est prêt ! Voici le lien ! 192.168.1.7:8888/'+lienUser+'.zip',
         };
 
         transporter.sendMail(mailOptions, function(error, info){
-        
+
             if (error) {
                 console.log(error);
             } else {
@@ -324,7 +383,7 @@ app.get('/dlplaylist', function(req, res) {
 
         transporter.close();
     }
-  
+
 
     function SupprimerLaPlaylistDuRepertoire(fileName) {
 
@@ -344,7 +403,7 @@ app.get('/dlplaylist', function(req, res) {
             }
         });
 
-        // Etape 2 : supprimer le repertoire 
+        // Etape 2 : supprimer le repertoire
         console.log("Function SupprimerLaPlaylistDuRepertoire etape 2 :")
 
         exec('rmdir  playlists/'+fileName, (err, stdout, stderr) => {
